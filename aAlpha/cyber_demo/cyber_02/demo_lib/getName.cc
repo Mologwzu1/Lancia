448dee1f09181975b8951a1ee23c7c99abf8d711
HJx <166993087+Mologwzu1@users.noreply.github.com>
2026-08-16 18:02:43 +0800

    Add files via upload

---
+++ aAlpha/cyber_demo/cyber_02/demo_lib/getName.cc
@@ -0,0 +1,5 @@
+// test_bazel/demo_lib/getName.cc
+#include "getName.h"
+std::string get_name(const std::string& name){
+    return name + ", attack!";
+}
---
+++ aAlpha/cyber_demo/cyber_02/demo_lib/getName.h
@@ -0,0 +1,4 @@
+// test_bazel/demo_lib/getName.h
+#pragma once
+#include <string>
+std::string get_name(const std::string& name);
